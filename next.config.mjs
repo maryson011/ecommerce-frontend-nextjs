a1ff83d521bead1cdcde964b25cf079454abbfec
maryson gleison <maryson011@gmail.com>
2024-07-18 14:28:17 -0300

    config - difinido data set

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['images.insplash.com']
+    }
+};
 
 export default nextConfig;
